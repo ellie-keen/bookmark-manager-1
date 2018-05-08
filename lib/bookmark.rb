e26330da501fdaf7c448f7f3f1e8ec8c5aa38323
Charly Mannion <charlottemannion100@gmail.com>
2018-05-08 14:43:19 +0100

    connected bookmarks database to model

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,9 +1,7 @@
 class Bookmark
   def self.all
-    [
-      "https://makersstudents.slack.com/",
-      "https://github.com/makersacademy/course",
-      "https://media1.tenor.com/images/170130993a5ce8d03f29415756393597/tenor.gif?itemid=7963714"
-    ]
+    connection = PG.connect(dbname: 'bookmark_manager')
+    result = connection.exec("SELECT * FROM bookmarks")
+    result.map { |bookmark| bookmark['url'] }
   end
 end
--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -1,8 +1,10 @@
+require 'pg'
+
 feature 'viewing bookmarks' do
   scenario 'displaying bookmarks' do
     visit '/bookmarks'
-    expect(page).to have_content("https://makersstudents.slack.com/")
-    expect(page).to have_content("https://github.com/makersacademy/course")
-    expect(page).to have_content("https://media1.tenor.com/images/170130993a5ce8d03f29415756393597/tenor.gif?itemid=7963714")
+    expect(page).to have_content("http://makersacademy.com")
+    expect(page).to have_content("http://google.com")
+    expect(page).to have_content("http://destroyallsoftware.com")
   end
 end
